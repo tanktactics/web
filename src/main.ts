4b1b686fcd9ecc1487d342b87a7c00b04ab5af3a
Martijn Faber <martijn@fabtronics.nl>
2021-07-28 10:50:02 +0200

    feat(i18n): add i18n

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { createApp } from "vue";
-import App from "./App.vue";
-import "./registerServiceWorker";
-import router from "./router";
-import store from "./store";
+import App from "@/App.vue";
+import "@/registerServiceWorker";
+
+import router from "@/router";
+import store from "@/store";
+import i18n from "@/i18n";
 
 import "@/scss/index.scss";
 
-createApp(App).use(store).use(router).mount("#app");
+createApp(App).use(i18n).use(store).use(router).mount("#app");
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,5 @@
 <template>
-  <h1>Hello world</h1>
+  <h1>{{ $t("message") }}</h1>
 </template>
 
 <script lang="ts">
