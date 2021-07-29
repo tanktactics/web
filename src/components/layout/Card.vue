d4c4ac840d021b3b4f0e61ec1d04f2f4c824cbcf
Martijn Faber <martijn@fabtronics.nl>
2021-07-29 15:13:30 +0200

    feat(card): add card component

---
+++ src/components/layout/Card.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="card">
+    <slot />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.card {
+  width: max-content;
+  border: 1px solid var(--border);
+  background-color: var(--content);
+  border-radius: var(--border-radius);
+  padding: var(--gap);
+  user-select: none;
+}
+</style>
