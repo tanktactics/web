9c00438096b4614921150493f20fa76d95a3f34f
Martijn Faber <martijn@fabtronics.nl>
2021-07-29 15:16:16 +0200

    feat(hovercard): add the hover card component

---
+++ src/components/base/HoverCard.vue
@@ -0,0 +1,48 @@
+<template>
+  <Card>
+    <h2>{{ username }}</h2>
+    <div class="info">
+      <p>{{ $t("points") }}: {{ points }}</p>
+      <p>{{ $t("health") }}: {{ health }}</p>
+      <p>{{ $t("range") }}: {{ range }}</p>
+    </div>
+  </Card>
+</template>
+
+<style lang="scss" scoped>
+.card {
+  position: absolute;
+  z-index: 1000;
+  margin-top: var(--gap);
+
+  .info {
+    color: var(--text-secondary);
+  }
+}
+</style>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import Card from "@/components/layout/Card.vue";
+
+export default defineComponent({
+  name: "HoverCard",
+  components: {
+    Card,
+  },
+  props: {
+    username: {
+      type: String,
+    },
+    points: {
+      type: Number,
+    },
+    health: {
+      type: Number,
+    },
+    range: {
+      type: Number,
+    },
+  },
+});
+</script>
